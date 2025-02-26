c190dba55cd89d1be8e9e6fe8516752d74d0f65b
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-02-26 00:10:24 -0800

    Added inputManager and PlayerController scripts, the character can move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float speed = 2f;
+    public InputManager inputManager;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        inputManager.OnMove.AddListener(MovePlayer);
+       
+    }
+
+    void MovePlayer(Vector2 input)
+    {
+        Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
+        playerRB.AddForce(inputXZPlane * speed);
+    }
+
+    
+
+   
+}
